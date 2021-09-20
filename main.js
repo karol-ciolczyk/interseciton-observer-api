324304abfe3e19a663099a9753b8fbc034d7b9ef
Karol Ciołczyk <dyweko@gmail.com>
2021-09-20 12:59:43 +0200

    solution

---
+++ main.js
@@ -0,0 +1,35 @@
+let isScrollUp = false;
+
+window.onscroll = function (e) {
+  isScrollUp = this.oldScroll > this.scrollY;
+  this.oldScroll = this.scrollY;
+};
+
+const targets = document.querySelectorAll("h2");
+
+const changeBackgroundColor = function (entries, observer) {
+  let randomNum1 = Math.random() * 255;
+  let randomNum2 = Math.random() * 255;
+
+  const topViewPortEvent = entries[0].intersectionRect.top;
+
+  if (isScrollUp && topViewPortEvent > 150) {
+    console.log("fired up");
+    document.body.style.backgroundColor = `rgba(140, ${randomNum1}, ${randomNum2}, 0.5)`;
+  }
+  if (!isScrollUp && topViewPortEvent > 150) {
+    console.log("fired down");
+    document.body.style.backgroundColor = `rgba(140, ${randomNum1}, ${randomNum2}, 0.5)`;
+  }
+};
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 1.0,
+};
+const observer = new IntersectionObserver(changeBackgroundColor, options);
+
+targets.forEach((header) => {
+  observer.observe(header);
+});
